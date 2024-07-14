92047efc31df61f1fdda637d12420833a7e475ed
Hafiz Mar <hafizmar@hotmail.co.uk>
2024-07-14 20:49:43 +0800

    ++update build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2654450833"
+    serviceWorkerVersion: "145214653"
   }
 });
